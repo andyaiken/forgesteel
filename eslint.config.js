d8910109935812f0a4edb2c705b345eb0f429c82
Andy Aiken <andy.aiken@live.co.uk>
2024-12-02 11:25:48 +0000

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -23,8 +22,7 @@ export default tseslint.config(
 			globals: globals.browser,
 		},
 		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
+			'react-hooks': reactHooks
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
@@ -115,10 +113,6 @@ export default tseslint.config(
 						'none'
 					]
 				}
-			],
-			'react-refresh/only-export-components': [
-				'warn',
-				{ allowConstantExport: true },
 			]
 		}
 	}

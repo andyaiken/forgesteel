6f5ea98f5e01bd8451196a303c3920a058663736
Primal Zed <primal.zed@gmail.com>
2025-01-01 10:42:44 -0600

    kebab-casing enum values and route segments

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,7 @@
 export class Format {
-	static capitalize = (text: string) => {
+	static capitalize = (text: string, separator: string = ' ') => {
 		return text
-			.split(' ')
+			.split(separator)
 			.filter(token => token.length > 0)
 			.map(token => `${token[0].toUpperCase()}${token.substring(1)}`)
 			.join(' ');

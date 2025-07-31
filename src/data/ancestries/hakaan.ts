d6cb21d3e9ac3ed5467b2f92e3e6bd0786434660
EjPlatzer <evan.platzer@gmail.com>
2025-07-31 07:50:39 -0400

    Update Hakaan ancestry for 1.0

--- src/data/ancestries/hakaan.ts
+++ src/data/ancestries/hakaan.ts
@@ -21,7 +21,7 @@ export const hakaan: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'hakaan-feature-2-1',
 						name: 'All Is A Feather',
-						description: 'You are exceptionally strong and have an edge made on tests that use the Lift skill.'
+						description: 'You are exceptionally strong. You gain an edge on tests made to lift and haul heavy objects.'
 					}),
 					value: 1
 				},
@@ -29,7 +29,7 @@ export const hakaan: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'hakaan-feature-2-2',
 						name: 'Forceful',
-						description: 'When you force move a creature or object, you can increase the distance moved by 1.'
+						description: 'Whenever you force move a creature or object, the forced movement distance gains a +1 bonus.'
 					}),
 					value: 1
 				},
@@ -37,7 +37,7 @@ export const hakaan: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'hakaan-feature-2-3',
 						name: 'Stand Tough',
-						description: 'Your body is made to withstand the blows of your enemies. Your Might score counts as 1 higher for resisting potencies.'
+						description: 'Your body is made to withstand the blows of your enemies. Your Might score is treated as 1 higher for resisting potencies, and you gain an edge on Might tests when called for to resist environmental effects or a creature’s traits or abilities.'
 					}),
 					value: 1
 				},
@@ -55,11 +55,11 @@ export const hakaan: Ancestry = {
 						id: 'hakaan-feature-2-5',
 						name: 'Doomsight',
 						description: `
-Working with your Director, you can predetermine an encounter in which you will die. When that encounter begins, you become doomed. While doomed, you automatically get tier 3 results on tests and ability power rolls, and you don’t die no matter how low your Stamina falls. You then die immediately at the end of the encounter.
+Working with your Director, you can predetermine an encounter in which you will die. When that encounter begins, you become doomed. While doomed, you automatically obtain a tier 3 outcome on tests and ability rolls, and you don’t die no matter how low your Stamina falls. You then die immediately at the end of the encounter, and can’t be returned to life by any means.
 
-If you don’t predetermine your death encounter, you can choose to become doomed while you are dying with the director’s approval (no action required). Doing so should be reserved for encounters in which you are dying as a result of suitable heroism, such as making a last stand against a boss or saving civilians, or when the consequences of your actions have finally caught up to you.
+If you don’t predetermine your death encounter, you can choose to become doomed while you are dying with the Director’s approval (no action required). Doing so should be reserved for encounters in which you are dying as a result of suitable heroism, such as making a last stand against a boss or saving civilians, or when the consequences of your actions have finally caught up to you — not because you’re playing a one-shot and have nothing to lose, Hacaarl.
 
-Additionally, when your Stamina equals the negative of your winded value and you are not doomed, you turn to rubble instead of experiencing death. You are unaware of your surroundings in this state. After 12 hours, you regain Stamina equal to your Recovery value.`
+Additionally, when your Stamina reaches the negative of your winded value and you are not doomed, you turn to rubble instead of experiencing death. You are unaware of your surroundings in this state, and you can’t regain Stamina or have this effect undone in any way. After 12 hours, you regain Stamina equal to your Recovery value.`
 					}),
 					value: 2
 				}

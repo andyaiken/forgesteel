e4156b18f9a8fd13d4de863e8e91159a3e82b578
Evan Platzer <evan.platzer@gmail.com>
2025-07-31 08:47:06 -0400

    Update dwarf ancestry for 1.0 (#360)

--- src/data/ancestries/dwarf.ts
+++ src/data/ancestries/dwarf.ts
@@ -12,11 +12,13 @@ export const dwarf: Ancestry = {
 			id: 'dwarf-feature-1',
 			name: 'Runic Carving',
 			description: `
-You can carve a rune onto your skin and the magic within your body activates it. The rune you carve determines the benefit you receive. You can change or remove this rune with 10 minutes of work while not engaged in combat.
+You can carve a rune onto your skin with 10 uninterrupted minutes of work, which is activated by the magic within your body. The rune you carve determines the benefit you receive. chosen from among the following:
 
-* **Detection**: Pick a specific type of creature, such as “goblins” or “humans” or an object, such as “magic swords” or “potions.” Your rune glows softly when you are within 20 squares of a chosen creature or object, regardless of line of effect. You can change the type of creature as a maneuver.
+* **Detection**: Pick a specific type of creature (such as “goblins” or “humans”) or object (such as “gems” or “potions”). Your rune glows softly when you are within 20 squares of any creature or object of thet type, even if you don’t have line of effect to the creature or object. You can change the type of creature or object as a maneuver.
 * **Light**: Your skin sheds light for 10 squares. You can turn this on and off as a maneuver.
-* **Voice**: As a maneuver, you can communicate telepathically with another willing creature you have met before whose name you know, who can speak and understand a language you know, and is within 1 mile of you. You and the creature can respond to one another as if having a normal conversation. You can change the person you communicate with by changing the rune.`
+* **Voice**: As a maneuver, you can communicate telepathically with a willing creature you have met before and who is within 1 mile of you. You must know the creature’s name, and they must speak and understand a language you know. You and the creature can respond to one another as if having a spoken conversation. You can communicate with a different creature by changing the rune.
+
+You can have one rune active at a time, and can change or remove a rune with 10 uninterrupted minutes of work.`
 		}),
 		FactoryLogic.feature.createChoice({
 			id: 'dwarf-feature-2',
@@ -26,7 +28,7 @@ You can carve a rune onto your skin and the magic within your body activates it.
 					feature: FactoryLogic.feature.createBonus({
 						id: 'dwarf-feature-2-1',
 						name: 'Grounded',
-						description: 'Your heavy stone body and connection to the earth makes it difficult for others to move you.',
+						description: 'Your heavy stone body and connection to the earth make it difficult for others to move you.',
 						field: FeatureField.Stability,
 						value: 1
 					}),
@@ -36,7 +38,7 @@ You can carve a rune onto your skin and the magic within your body activates it.
 					feature: FactoryLogic.feature.create({
 						id: 'dwarf-feature-2-2',
 						name: 'Stand Tough',
-						description: 'Your body is made to withstand the blows of your enemies. Your Might counts as 1 higher for resisting potencies.'
+						description: 'Your body is made to withstand the blows of your enemies. Your Might score is treated as 1 higher for resisting potencies, and you gain an edge on Might tests when called for to resist environmental effects or a creature’s traits or abilities.'
 					}),
 					value: 1
 				},
@@ -44,7 +46,7 @@ You can carve a rune onto your skin and the magic within your body activates it.
 					feature: FactoryLogic.feature.create({
 						id: 'dwarf-feature-2-3',
 						name: 'Stone Singer',
-						description: 'You have a magic connection to the earth. You can spend 1 uninterrupted hour singing and reshape any unworked, mundane stone within 3 squares of you. You can’t destroy this stone, but you can move each square of it anywhere within 3 squares of you, piling it off to one side to dig a hole or building it all up to create a wall.'
+						description: 'You have a magic connection to the earth. When you spend 1 uninterrupted hour singing, you can reshape any unworked mundane stone within 3 squares. You can’t destroy this stone, but you can move each square of it anywhere within 3 squares of you, piling it off to one side to dig a hole or building it up to create a wall.'
 					}),
 					value: 1
 				},
@@ -64,7 +66,7 @@ You can carve a rune onto your skin and the magic within your body activates it.
 						description: 'Your stone skin affords you potent protection.',
 						field: FeatureField.Stamina,
 						value: 6,
-						valuePerLevel: 3
+						valuePerEchelon: 6
 					}),
 					value: 2
 				}

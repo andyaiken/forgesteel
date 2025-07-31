baedb3cd755752fef61dbefa8599f9d8cb638834
Thuunda <87797905+Thuunda@users.noreply.github.com>
2025-08-01 08:22:36 +1000

    Update whirlwind.ts

--- src/data/kits/whirlwind.ts
+++ src/data/kits/whirlwind.ts
@@ -7,7 +7,7 @@ import { KitWeapon } from '../../enums/kit-weapon';
 export const whirlwind: Kit = {
 	id: 'kit-whirlwind',
 	name: 'Whirlwind',
-	description: 'The Whirlwind kit makes effective use of whips, granting you mobility, damage, and reach. If you want to be a mobile warrior who uses a chain or whip, then this is the kit for you.',
+	description: 'The Whirlwind kit makes effective use of whips, granting you mobility, damage, and reach. If you want to be a fast-moving warrior who lashes foes with a chain or whip, then this is the kit for you.',
 	type: '',
 	armor: [],
 	weapon: [ KitWeapon.Whip ],
@@ -24,19 +24,19 @@ export const whirlwind: Kit = {
 			ability: FactoryLogic.createAbility({
 				id: 'kit-whirlwind-signature',
 				name: 'Extension Of My Arm',
-				description: 'When you draw your whip back after an attack, your enemy comes ever closer.',
+				description: 'When you draw your whip back after an attack, your enemy is drawn ever closer.',
 				type: FactoryLogic.type.createAction(),
 				keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
-				distance: [ FactoryLogic.distance.createMelee(2) ],
+				distance: [ FactoryLogic.distance.createMelee(3) ],
 				target: '1 creature',
 				cost: 'signature',
 				sections: [
 					FactoryLogic.createAbilitySectionRoll(
 						FactoryLogic.createPowerRoll({
 							characteristic: [ Characteristic.Might, Characteristic.Agility ],
-							tier1: '3 + M or A damage; vertical pull 1',
-							tier2: '6 + M or A damage; vertical pull 2',
-							tier3: '9 + M or A damage; vertical pull 3'
+							tier1: '4 + M or A damage; vertical pull 1',
+							tier2: '7 + M or A damage; vertical pull 2',
+							tier3: '10 + M or A damage; vertical pull 3'
 						})
 					)
 				]

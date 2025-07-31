77ea4a1ae08800386b08462b0aa713a125be9b80
limerich <liam.richards94@gmail.com>
2025-07-31 14:52:01 +0100

    Update elf-wode.ts (#363)

--- src/data/ancestries/elf-wode.ts
+++ src/data/ancestries/elf-wode.ts
@@ -10,8 +10,8 @@ export const wodeElf: Ancestry = {
 	features: [
 		FactoryLogic.feature.create({
 			id: 'wode-elf-feature-1',
-			name: 'Wode Elf Glamor',
-			description: 'You can magically alter your appearance to better blend in with your surroundings. You gain an edge on Agility tests made to hide and sneak, and tests made to find you while you are hidden take a bane.'
+			name: 'Signature Trait: Wode Elf Glamor',
+			description: 'You can magically alter your appearance to better blend in with your surroundings. You gain an edge on tests made to hide and sneak, and tests made to search for you while you are hidden take a bane.'
 		}),
 		FactoryLogic.feature.createChoice({
 			id: 'wode-elf-feature-2',
@@ -21,7 +21,7 @@ export const wodeElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'wode-elf-feature-2-1',
 						name: 'Forest Walk',
-						description: 'You can shift into difficult terrain.'
+						description: 'You can shift into and while within difficult terrain.'
 					}),
 					value: 1
 				},
@@ -29,7 +29,7 @@ export const wodeElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'wode-elf-feature-2-2',
 						name: 'Revisit Memory',
-						description: 'Accessing memories is as easy as living in the present for you. You have an edge on all tests made to recall lore.'
+						description: 'Accessing memories is as easy as living in the present for you. You gain an edge on tests made to recall lore.'
 					}),
 					value: 1
 				},
@@ -45,7 +45,7 @@ export const wodeElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'wode-elf-feature-2-4',
 						name: 'Otherworldly Grace',
-						description: 'Your elf body and mind can’t be contained for long. You succeed on saving throws when you get a 5 or higher.'
+						description: 'Your elf body and mind can’t be contained for long. Whenever you make a saving throw, you succeed on a roll of 5 or higher.'
 					}),
 					value: 2
 				},
@@ -58,7 +58,7 @@ export const wodeElf: Ancestry = {
 							type: FactoryLogic.type.createAction(),
 							keywords: [ AbilityKeyword.Magic, AbilityKeyword.Ranged, AbilityKeyword.Strike ],
 							distance: [ FactoryLogic.distance.createRanged(10) ],
-							target: '1 creature',
+							target: 'One creature',
 							cost: 'signature',
 							sections: [
 								FactoryLogic.createAbilitySectionRoll(
@@ -66,13 +66,21 @@ export const wodeElf: Ancestry = {
 										characteristic: [ Characteristic.Might, Characteristic.Agility ],
 										tier1: '2 + M or A damage; A < [weak] slowed (save ends)',
 										tier2: '3 + M or A damage; A < [average] slowed (save ends)',
-										tier3: '5 + M or A damage; A < [strong] restrained (save ends)'
+										tier3: '5 + M or A damage; A < [strong] slowed (save ends)'
 									})
 								)
 							]
 						})
 					}),
 					value: 2
+				},
+				{
+					feature: FactoryLogic.feature.create({
+						id: 'wode-elf-feature-2-6',
+						name: 'Quick and Brutal',
+						description: 'Whenever you score a critical hit, you can take an additional main action and an additional move action instead of just a main action.'
+					}),
+					value: 1
 				}
 			],
 			count: 3

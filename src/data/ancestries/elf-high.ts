076ce2f2253591b7de191291650318d4997d6b2e
EjPlatzer <evan.platzer@gmail.com>
2025-07-31 21:34:32 -0400

    Update high elf ancestry

--- src/data/ancestries/elf-high.ts
+++ src/data/ancestries/elf-high.ts
@@ -6,12 +6,12 @@ import { FeatureField } from '../../enums/feature-field';
 export const highElf: Ancestry = {
 	id: 'ancestry-high-elf',
 	name: 'Elf (high)',
-	description: 'Children of the solar celestials created to tend their libraries and attend to the true elves as heralds, the high elves remember a better age, before the coming of humans and war. A time when the celestials were still in the world, and all that mattered was art and beauty.',
+	description: 'Children of the solar celestials created to tend their libraries and attend to the true elves as heralds, the high elf history describes a better age, before the coming of humans and war. A time when the celestials were still in the world, and all that mattered was art and beauty.',
 	features: [
 		FactoryLogic.feature.create({
 			id: 'high-elf-feature-1',
 			name: 'High Elf Glamor',
-			description: 'A magic glamor makes others perceive you as interesting and engaging, granting you an edge on Presence tests using the Flirt or Persuade skills. This glamor makes you look and sound slightly different to each creature you meet, since what is engaging to one might be different for another. However, you never appear to be anyone other than yourself.'
+			description: 'A magic glamor makes others perceive you as interesting and engaging, granting you an edge on Presence tests using the Flirt or Persuade skills. This glamor makes you appear and sound slightly different to each creature you meet, since what is engaging to one might be different for another. However, you never appear to be anyone other than yourself.'
 		}),
 		FactoryLogic.feature.createChoice({
 			id: 'high-elf-feature-2',
@@ -21,7 +21,7 @@ export const highElf: Ancestry = {
 					feature: FactoryLogic.feature.createBonus({
 						id: 'high-elf-feature-2-1',
 						name: 'Graceful Retreat',
-						description: 'When you take the Disengage move action, you can shift 1 additional square as part of the move action.',
+						description: 'You gain a +1 bonus to the distance you can shift when you take the Disengage move action.',
 						field: FeatureField.Disengage,
 						value: 1
 					}),
@@ -31,7 +31,7 @@ export const highElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'high-elf-feature-2-2',
 						name: 'High Senses',
-						description: 'You have senses that are keen and perceptive. You have an edge on Intuition tests that use the Awareness skill.'
+						description: 'Your senses are especially keen and perceptive. You gain an edge on tests made to notice threats.'
 					}),
 					value: 1
 				},
@@ -39,7 +39,7 @@ export const highElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'high-elf-feature-2-3',
 						name: 'Revisit Memory',
-						description: 'Accessing memories is as easy as living in the present for you. You have an edge on all tests made to recall lore.'
+						description: 'Accessing memories is as easy as living in the present for you. You gain an edge on tests made to recall lore.'
 					}),
 					value: 1
 				},
@@ -47,7 +47,7 @@ export const highElf: Ancestry = {
 					feature: FactoryLogic.feature.create({
 						id: 'high-elf-feature-2-4',
 						name: 'Otherwordly Grace',
-						description: 'Your elf body and mind can’t be contained for long. You succeed on saving throws when you get a 5 or higher.'
+						description: 'Your elf body and mind can’t be contained for long. Whenever you make a saving throw, you succeed on a roll of 5 or higher.'
 					}),
 					value: 2
 				},

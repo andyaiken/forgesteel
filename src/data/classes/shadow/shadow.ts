5ac02a34b14c6876ae2a989814a478ee35e44d41
cort-of-keo <kolsen8@gmail.com>
2025-08-01 12:47:47 -0700

    Base class up to level 5

--- src/data/classes/shadow/shadow.ts
+++ src/data/classes/shadow/shadow.ts
@@ -1,3 +1,4 @@
+import { AbilityDistanceType } from '../../../enums/abiity-distance-type';
 import { AbilityKeyword } from '../../../enums/ability-keyword';
 import { Characteristic } from '../../../enums/characteristic';
 import { FactoryLogic } from '../../../logic/factory-logic';
@@ -13,9 +14,9 @@ export const shadow: HeroClass = {
 	id: 'class-shadow',
 	name: 'Shadow',
 	description: `
-Subtlety is your art, the tip of the blade your brush. You studied at a secret college, specializing in alchemy, illusion, or shadow-magics. Your training and knowledge places you among the elite assassins, spies, and commandos. But more powerful than any weapon or sorcery is your insight into your enemies’ weaknesses.
+Subtlety is your art, the tip of the blade your brush. You studied at a secret college, specializing in alchemy, illusion, or shadow-magics. Your training and knowledge place you among the elite ranks of assassins, spies, and commandos. But more potent than any weapon or sorcery is your insight into your enemies’ weaknesses.
 
-As a shadow, you have abilities that deal a lot of damage, let you move swiftly across the battlefield and away from hazards, and allow you to fade from notice even in the middle of the most heated combat encounter. You also possess more skills than any other hero.`,
+As a shadow, you possess abilities that deal significant damage, enable you to move swiftly across the battlefield and evade hazards, and allow you to fade from notice even in the midstof the most intense combat encounters. You also possess more skills than any other hero.`,
 	subclassName: 'Shadow College',
 	subclassCount: 1,
 	primaryCharacteristicsOptions: [
@@ -30,7 +31,7 @@ As a shadow, you have abilities that deal a lot of damage, let you move swiftly 
 					id: 'shadow-stamina',
 					field: FeatureField.Stamina,
 					value: 18,
-					valuePerLevel: 9
+					valuePerLevel: 6
 				}),
 				FactoryLogic.feature.createBonus({
 					id: 'shadow-recoveries',
@@ -129,6 +130,113 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 					cost: 7
 				})
 			]
+		},
+		{
+			level: 4,
+			features: [
+				FactoryLogic.feature.createCharacteristicBonus({
+					id: 'shadow-4-1a',
+					name: 'Characteristic Increase: Agility',
+					description: 'Your Agility score increases to 3',
+					characteristic:Characteristic.Agility,
+					value: 1
+				}),
+				FactoryLogic.feature.createChoice({
+					id: 'shadow-4-1b',
+					name: 'Characteristic Increase: Additional',
+					description: 'Additionally, you can increase one of your characteristic scores by 1, to a maximum of 3.',
+					options: [
+						{
+							feature: FactoryLogic.feature.createCharacteristicBonus({
+								id: 'shadow-4-1b-1',
+								characteristic: Characteristic.Might,
+								value: 1
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createCharacteristicBonus({
+								id: 'shadow-4-1b-2',
+								characteristic: Characteristic.Reason,
+								value: 1
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createCharacteristicBonus({
+								id: 'shadow-4-1b-3',
+								characteristic: Characteristic.Intuition,
+								value: 1
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createCharacteristicBonus({
+								id: 'shadow-4-1b-4',
+								characteristic: Characteristic.Presence,
+								value: 1
+							}),
+							value: 1
+						}
+					]
+				}),
+				FactoryLogic.feature.create({
+					id: 'shadow-4-2',
+					name: 'Keep It Down',
+					description: 'While conversing with any creature you share a language with, you can decide whether anyone else can perceive what you’re conveying, even while yelling.'
+				}),
+				FactoryLogic.feature.createAbility({
+					ability: FactoryLogic.createAbility({
+						id: 'shadow-4-3',
+						name: 'Night Watch',
+						description: 'A steely dagger from out of the blue knocks another weapon off course.',
+						type: FactoryLogic.type.createTrigger('The target takes damage from another creature’s ability while you are hidden.'),
+						keywords: [AbilityKeyword.Ranged, AbilityKeyword.Weapon],
+						distance: [ FactoryLogic.distance.createRanged(5) ],
+						target: '1 ally',
+						sections: [
+							FactoryLogic.createAbilitySectionText('The target takes half the damage. You remain hidden.')
+						]
+					})
+				}),
+				FactoryLogic.feature.createPerk({
+					id: 'shadow-4-4'
+				}),
+				FactoryLogic.feature.createSkillChoice({
+					id: 'shadow-4-5',
+					listOptions: [ SkillList.Crafting, SkillList.Exploration, SkillList.Interpersonal, SkillList.Intrigue, SkillList.Lore ],
+					count: 1
+				}),
+				// Is this the right way?
+				
+				FactoryLogic.feature.createHeroicResource({
+					id: 'shadow-resource-4',
+					name: 'Insight',
+					gains: [
+						{
+							trigger: 'Start of your turn',
+							value: '1d3'
+						},
+						{
+							trigger: 'The first time each round that you deal damage with at least one surge',
+							value: '2'
+						}
+					],
+					details: `
+When you use a heroic ability that has a power roll, that ability costs 1 less insight if you have an edge or double edge on it.
+
+If the ability has multiple targets, the cost is reduced even if the ability has an edge or double edge against only one target.`
+				})
+			]
+		},
+		{
+			level: 5,
+			features: [
+					FactoryLogic.feature.createClassAbilityChoice({
+					id: 'shadow-5-1',
+					cost: 9
+				})
+			]
 		}
 	],
 	abilities: [
@@ -150,7 +258,7 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 						tier3: '8 + A damage; I < [strong], prone'
 					})
 				),
-				FactoryLogic.createAbilitySectionText('An ally of your choice within 5 squares of the target gains a surge.')
+				FactoryLogic.createAbilitySectionText('One ally of your choice within 5 squares of the target gains 1 surge.')
 			]
 		}),
 		FactoryLogic.createAbility({
@@ -174,13 +282,13 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 						tier3: '9 + A damage'
 					})
 				),
-				FactoryLogic.createAbilitySectionText('If the target has no allies adjacent to them, this strike deals extra damage equal to your Agility score.')
+				FactoryLogic.createAbilitySectionText('If the target has no allies adjacent to them, you gain 1 surge before making the power roll.')
 			]
 		}),
 		FactoryLogic.createAbility({
 			id: 'shadow-ability-3',
 			name: 'Teamwork Has Its Place',
-			description: 'You attack an enemy, distracting them long enough for an ally to stab them.',
+			description: 'You attack an enemy as an ally exposes their weakness.',
 			type: FactoryLogic.type.createMain(),
 			keywords: [ AbilityKeyword.Melee, AbilityKeyword.Ranged, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
 			distance: [
@@ -198,7 +306,7 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 						tier3: '9 + A damage'
 					})
 				),
-				FactoryLogic.createAbilitySectionText('If an ally is adjacent to the target, the target takes extra damage equal to your Agility score.')
+				FactoryLogic.createAbilitySectionText('If any ally is adjacent to the target, you gain 1 surge before making the power roll.')
 			]
 		}),
 		FactoryLogic.createAbility({
@@ -219,7 +327,7 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 						tier3: '8 + A damage'
 					})
 				),
-				FactoryLogic.createAbilitySectionText('As long as you have at least one ally within 5 squares of the target, you gain a surge. If you are flanking the target when you use this ability, choose one ally who is flanking with you. That ally also gain a surge.')
+				FactoryLogic.createAbilitySectionText('As long as you have one or more allies within 5 squares of the target, you gain 1 surge. If you are flanking the target when you use this ability, choose one ally who is flanking with you. That ally also gains 1 surge.')
 			]
 		}),
 		FactoryLogic.createAbility({
@@ -321,9 +429,9 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 			sections: [
 				FactoryLogic.createAbilitySectionRoll(FactoryLogic.createPowerRoll({
 					characteristic: [ Characteristic.Agility ],
-					tier1: '1d6 + 7 + A damage',
-					tier2: '1d6 + 11 + A damage',
-					tier3: '1d6 + 16 + A damage'
+					tier1: '2d6 + 7 + A damage',
+					tier2: '2d6 + 11 + A damage',
+					tier3: '2d6 + 16 + A damage'
 				}))
 			]
 		}),
@@ -332,25 +440,25 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 			name: 'One Hundred Throats',
 			description: 'As you move across the battlefield, every foe within reach feels your wrath.',
 			type: FactoryLogic.type.createMain(),
-			keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+			keywords: [ AbilityKeyword.Melee, AbilityKeyword.Weapon ],
 			distance: [ FactoryLogic.distance.createSelf() ],
 			target: 'Self',
 			cost: 5,
 			sections: [
-				FactoryLogic.createAbilitySectionText('You shift up to your speed. You make one power roll that targets up to three enemies, each of who became adjacent to you during the move.'),
+				FactoryLogic.createAbilitySectionText('You shift up to your speed and make one power roll that targets up to three enemies who came adjacent to you during the move.'),
 				FactoryLogic.createAbilitySectionRoll(
 					FactoryLogic.createPowerRoll({
 						characteristic: [ Characteristic.Agility ],
 						tier1: '3 damage',
 						tier2: '6 damage',
-						tier3: '9 + A damage'
+						tier3: '9 damage'
 					})
 				)
 			]
 		}),
 		FactoryLogic.createAbility({
 			id: 'shadow-ability-11',
-			name: 'Set-Up',
+			name: 'Setup',
 			description: 'Your friends will thank you.',
 			type: FactoryLogic.type.createMain(),
 			keywords: [ AbilityKeyword.Ranged, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
@@ -376,7 +484,7 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 			target: 'Self',
 			cost: 5,
 			sections: [
-				FactoryLogic.createAbilitySectionText('You make two signature strikes.')
+				FactoryLogic.createAbilitySectionText('You use a strike signature ability twice.')
 			]
 		}),
 		FactoryLogic.createAbility({
@@ -389,7 +497,7 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 			target: 'Self',
 			cost: 7,
 			sections: [
-				FactoryLogic.createAbilitySectionText('Until the end of the encounter, whenever an enemy moves adjacent to you or damages you, you can take the Disengage move action as a free triggered action.')
+				FactoryLogic.createAbilitySectionText('Until the end of the encounter, whenever an enemy moves or is force moved adjacent to you or damages you, you can take the Disengage move action as a free triggered action.')
 			]
 		}),
 		FactoryLogic.createAbility({
@@ -453,6 +561,81 @@ If the ability has multiple targets, the cost is reduced even if the ability has
 					tier2: '11 + A damage; M < [average], prone and can’t stand (save ends)',
 					tier3: '16 + A damage; M < [strong], prone and can’t stand (save ends)'
 				}))
+			]
+		}),
+		FactoryLogic.createAbility({
+			id: 'shadow-ability-17',
+			name: 'Blackout',
+			description: 'You cause a plume of shadow to erupt from your eyes and create a cloud of darkness.',
+			type: FactoryLogic.type.createManeuver(),
+			keywords: [AbilityKeyword.Area, AbilityKeyword.Magic],
+			distance: [ FactoryLogic.distance.create({ type: AbilityDistanceType.Burst, value: 3 }) ],
+			target: 'Self',
+			cost: 9,
+			sections: [
+				FactoryLogic.createAbilitySectionText('A black cloud fills the area until the end of your next turn, granting you and your allies concealment against enemies. While you are in the area, whenever an enemy ends their turn in the area, you can use a free triggered action to shift to a new location within the area and make a free strike against them.')
+			]
+		}),
+		FactoryLogic.createAbility({
+			id: 'shadow-ability-18',
+			name: 'Into the Shadows',
+			description: 'You sweep your foe off their feet and plunge them into absolute darkness.',
+			type: FactoryLogic.type.createMain(),
+			keywords: [ AbilityKeyword.Magic, AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+			distance: [ FactoryLogic.distance.createMelee() ],
+			target: '1 creature or object',
+			cost: 9,
+			sections: [
+				FactoryLogic.createAbilitySectionText('You and the target are removed from the encounter map until the start of your next turn. You reappear in the spaces you left or the nearest unoccupied spaces. Make a power roll upon your return.'),
+				FactoryLogic.createAbilitySectionRoll(FactoryLogic.createPowerRoll({
+					characteristic: [ Characteristic.Agility ],
+					tier1: '8 + A corruption damage',
+					tier2: '13 + A corruption damage',
+					tier3: '17 + A corruption damage'
+				}))
+			]
+		}),
+		FactoryLogic.createAbility({
+			id: 'shadow-ability-19',
+			name: 'Shadowfall',
+			description: 'You vanish. They fall. You reappear.',
+			type: FactoryLogic.type.createMain(),
+			keywords: [ AbilityKeyword.Area, AbilityKeyword.Melee, AbilityKeyword.Weapon ],
+			distance: [ FactoryLogic.distance.create({ type: AbilityDistanceType.Line, value: 10, within: 1 }) ],
+			target: 'Each enemy in area',
+			cost: 9,
+			sections: [
+				FactoryLogic.createAbilitySectionRoll(FactoryLogic.createPowerRoll({
+					characteristic: [ Characteristic.Agility ],
+					tier1: '10 damage',
+					tier2: '14 damage',
+					tier3: '20 damage'
+				})),
+				FactoryLogic.createAbilitySectionText('You disappear before making the power roll. After the power roll is resolved, you appear in the first unoccupied space at the far end of the line.'),
+
+			]
+		}),
+		FactoryLogic.createAbility({
+			id: 'shadow-ability-20',
+			name: 'You Talk Too Much',
+			description: 'Silence is a virtue. A knife pinning their mouth shut is the next best thing.',
+			type: FactoryLogic.type.createMain(),
+			keywords: [ AbilityKeyword.Melee, AbilityKeyword.Ranged, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+			distance: [
+				FactoryLogic.distance.createMelee(),
+				FactoryLogic.distance.createRanged(5)
+			],
+			target: '1 creature',
+			cost: 9,
+			sections: [
+				FactoryLogic.createAbilitySectionRoll(FactoryLogic.createPowerRoll({
+					characteristic: [ Characteristic.Agility ],
+					tier1: '10 + A damage; P < [weak], dazed (save ends)',
+					tier2: '15 + A damage; P < [average], dazed (save ends)',
+					tier3: '21 + A damage; P < [strong], dazed (save ends)'
+				})),
+				FactoryLogic.createAbilitySectionText('The target can’t communicate with anyone until the end of the encounter.'),
+
 			]
 		})
 	],

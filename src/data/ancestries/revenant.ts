b7d31a29adcaa6a43182080bc8abc903fe85e367
Evan Platzer <evan.platzer@gmail.com>
2025-08-02 03:28:12 -0400

    Update revenant ancestry (#369)
    The Revenant's purchase points are supposed to be 2 or 3 if the former ancestry is size 1S, but I'm not sure how to encode that (or if the current logic even allows that kind of dependency).

--- src/data/ancestries/revenant.ts
+++ src/data/ancestries/revenant.ts
@@ -9,12 +9,12 @@ import { FactoryLogic } from '../../logic/factory-logic';
 export const revenant: Ancestry = {
 	id: 'ancestry-revenant',
 	name: 'Revenant',
-	description: 'Unlike the necromantic rituals that produce wights and wraiths and zombies, revenants rise from the grave through a combination of an unjust death and a burning desire for vengeance. Creatures sustained on pure will, they have no need of food or water or air - and, unlike their zombified cousins, they retain all their memories and personality from life.',
+	description: 'The dead walk among us. Some of them are happier about it than others. Unlike the necromantic rituals that produce wights and wraiths and zombies, revenants rise from the grave through a combination of an unjust death and a burning desire for vengeance. Creatures sustained on pure will, they have no need of food or water or air - and, unlike their zombified cousins, they retain all their memories and personality from life.',
 	features: [
 		FactoryLogic.feature.createAncestry({
 			id: 'revenant-feature-1',
 			name: 'Former Life',
-			description: 'Choose the ancestry you were before you died.'
+			description: 'Choose the ancestry you were before you died. Your size is that ancestry’s size and your speed is 5. Unless you select one of the Previous Life traits (see below), you don’t receive any other ancestral traits from your original ancestry.'
 		}),
 		FactoryLogic.feature.createDamageModifier({
 			id: 'revenant-feature-2',
@@ -29,7 +29,7 @@ export const revenant: Ancestry = {
 		FactoryLogic.feature.create({
 			id: 'revenant-feature-3',
 			name: 'Tough But Withered',
-			description: 'When your Stamina equals the negative of your winded value, you become inert instead of dying. You can continue to observe your surroundings, but you can’t speak, take actions, maneuvers, or triggered actions, or move and you fall prone. If you take any fire damage while in this state, your body is destroyed and you die. Otherwise, after 12 hours, you regain Stamina equal to your recovery value.'
+			description: 'When your Stamina reaches the negative of your winded value, you become inert instead of dying. You fall prone and can’t stand. You continue to observe your surroundings, but you can’t speak, take main actions, maneuvers, move actions, or triggered actions. While inert this way, if you take any fire damage, your body is destroyed and you die. Otherwise, after 12 hours, you regain Stamina equal to your recovery value.'
 		}),
 		FactoryLogic.feature.createChoice({
 			id: 'revenant-feature-4',
@@ -83,19 +83,17 @@ export const revenant: Ancestry = {
 								description: `
 As a maneuver, you place a magic sigil on a creature within 10 squares of you. When you place a sigil, you can decide where it appears on the creature’s body, and whether the sigil is visible to only you or to all creatures.
 
-You always know the direction to the exact location of a creature who bears one of your sigils and is on the same world as you.
-
-You can have an active number of sigils equal to your level. You can remove a sigil from a creature harmlessly (no action required). If you are already using your maximum number of sigils and place a new one, your oldest sigil disappears with no other effect.`
+You always know the direction to the exact location of a creature who bears one of your sigils and is on the same world. You can have an active number of sigils equal to your level, and can remove a sigil from a creature at will (no action required). If you already have the maximum number of sigils activated and you place a new one, your oldest sigil disappears with no other effect.`
 							}),
 							FactoryLogic.feature.createAbility({
 								ability: FactoryLogic.createAbility({
 									id: 'revenant-feature-4-5-2',
 									name: 'Detonate Sigil',
-									description: 'A magical sigil you placed on a creature explodes with energy.',
+									description: 'A magic sigil you placed on a creature explodes with energy.',
 									type: FactoryLogic.type.createMain(),
 									keywords: [ AbilityKeyword.Magic, AbilityKeyword.Ranged, AbilityKeyword.Strike ],
 									distance: [ FactoryLogic.distance.createRanged(10) ],
-									target: '1 creature with your sigil',
+									target: 'One creature bearing your sigil',
 									cost: 'signature',
 									sections: [
 										FactoryLogic.createAbilitySectionRoll(
@@ -115,7 +113,7 @@ You can have an active number of sigils equal to your level. You can remove a si
 					value: 2
 				}
 			],
-			count: 2
+			count: 2 // This should be '2 or 3 if size is 1S', but I don't know how to encode that. 
 		})
 	]
 };

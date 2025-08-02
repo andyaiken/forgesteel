85053ce23d6f1fd3cf2ecb91205edf5e1c33b490
NamelessYoungy <young.nathan.t@gmail.com>
2025-08-02 22:17:14 +1000

    Update Fury (#380)

--- src/data/classes/fury/berserker.ts
+++ src/data/classes/fury/berserker.ts
@@ -1,3 +1,4 @@
+import { AbilityDistanceType } from '../../../enums/abiity-distance-type';
 import { AbilityKeyword } from '../../../enums/ability-keyword';
 import { Characteristic } from '../../../enums/characteristic';
 import { FactoryLogic } from '../../../logic/factory-logic';
@@ -25,13 +26,13 @@ export const berserker: SubClass = {
 					id: 'fury-sub-1-1-3',
 					name: 'Primordial Strength',
 					description: `
-Whenever you damage an object with a weapon strike, it takes additional damage equal to your Might score. Additionally, whenever you push another creature into an object, they take additional damage equal to your Might score.
+Whenever you damage an object with a weapon strike, the strike deals extra damage equal to your Might score. Additionally, whenever you push another creature into an object, the creature takes extra damage equal to your Might score.
 
-As your rage grows, your primordial strength intensifies. Benefits are cumulative except where an improved benefit replaces a lesser benefit.
+As your ferocity grows, you gain benefits as noted on the Berserker Growing Ferocity table. Benefits are cumulative except where an improved benefit replaces a lesser benefit.
 
-* **Rage 2**: Add your Might to the distance you achieve on the Knockback maneuver.
-* **Rage 4**: Gain one surge the first time on a turn that you push a creature.
-* **Rage 6**: Gain an edge on Might tests and the Knockback maneuver.`
+* **Ferocity 2**: Whenever you use the Knockback maneuver, the forced movement distance gains a bonus equal to your Might score.
+* **Ferocity 4**: The first time you push a creature on a turn, you gain 1 surge.
+* **Ferocity 6**: You gain an edge on Might tests and the Knockback maneuver.`
 				}),
 				FactoryLogic.feature.createAbility({
 					ability: FactoryLogic.createAbility({
@@ -43,11 +44,11 @@ As your rage grows, your primordial strength intensifies. Benefits are cumulativ
 						distance: [ FactoryLogic.distance.createMelee() ],
 						target: 'Self or 1 creature',
 						sections: [
-							FactoryLogic.createAbilitySectionText('You can select a new target of the same size or smaller within distance to be force moved instead, and you can turn that forced movement into a push instead. You become the source of the forced movement and decide where the new target’s destination. Additionally, the forced movement distance gains a bonus equal to your Might score.'),
+							FactoryLogic.createAbilitySectionText('You can select a new target of the same size or smaller within distance to be force moved instead. You become the source of the forced movement, determine the new target’s destination, and can push the target instead of using the original forced movement type. Additionally, the forced movement distance gains a bonus equal to your Might score.'),
 							FactoryLogic.createAbilitySectionField({
 								name: 'Spend',
 								value: 1,
-								effect: 'The forced movement distance instead gains a bonus equal to twice your Might score.'
+								effect: 'The forced movement distance gains a bonus equal to twice your Might score instead.'
 							})
 						]
 					})
@@ -60,7 +61,7 @@ As your rage grows, your primordial strength intensifies. Benefits are cumulativ
 				FactoryLogic.feature.create({
 					id: 'fury-sub-1-2-1',
 					name: 'Unstoppable Force',
-					description: 'Whenever you use the Charge action, you can make a signature strike or a heroic ability melee strike instead of a free strike. Additionally, you can jump as part of a charge.'
+					description: 'Whenever you use the Charge main action, you can use a strike signature ability or a strike heroic ability instead of a free strike. Additionally, you can jump as part of your charge.'
 				}),
 				FactoryLogic.feature.createChoice({
 					id: 'fury-sub-1-2-2',
@@ -72,12 +73,12 @@ As your rage grows, your primordial strength intensifies. Benefits are cumulativ
 									name: 'Special Delivery',
 									description: 'You ready?',
 									type: FactoryLogic.type.createManeuver(),
-									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Weapon ],
 									distance: [ FactoryLogic.distance.createMelee() ],
 									target: 'One willing ally',
 									cost: 5,
 									sections: [
-										FactoryLogic.createAbilitySectionText('You vertically push the target up to 4 squares. This forced movement ignores the target’s stability, and the target takes no damage from the move. At the end of this movement, the target can make a free strike that deals additional damage equal to your Might score.')
+										FactoryLogic.createAbilitySectionText('You vertically push the target up to 4 squares. This forced movement ignores the target’s stability, and the target takes no damage from colliding with creatures or objects. At the end of this movement, the target can make a free strike that deals extra damage equal to your Might score.')
 									]
 								})
 							}),
@@ -88,17 +89,17 @@ As your rage grows, your primordial strength intensifies. Benefits are cumulativ
 								ability: FactoryLogic.createAbility({
 									id: 'fury-sub-1-2-2b',
 									name: 'Wrecking Ball',
-									description: 'It is easier to destroy than to create. Much easier, in fact!',
+									description: 'It’s easier to destroy than to create. Much easier, in fact!',
 									type: FactoryLogic.type.createManeuver(),
 									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Weapon ],
 									distance: [ FactoryLogic.distance.createSelf() ],
-									target: 'Self',
+									target: 'Self; see below',
 									cost: 5,
 									sections: [
 										FactoryLogic.createAbilitySectionText(`
 You move up to your speed in a straight line. During this movement, you can move through mundane structures, including walls, which are difficult terrain for you. You automatically destroy each square of structure you move through and leave behind a square of difficult terrain.
 
-Additionally, you make one power roll that targets each enemy you come adjacent to during the move.`),
+Additionally, you make one power roll that targets each enemy you move adjacent to during this movement.`),
 										FactoryLogic.createAbilitySectionRoll(
 											FactoryLogic.createPowerRoll({
 												characteristic: [ Characteristic.Might ],
@@ -122,12 +123,186 @@ Additionally, you make one power roll that targets each enemy you come adjacent 
 				FactoryLogic.feature.create({
 					id: 'fury-sub-1-3-1',
 					name: 'Immovable Object',
-					description: 'You add your level to your effective size for the purpose of interacting with creatures and objects, including determining whether you can lift an object, are affected by forced movement, and so forth. This has no effect on your ability to be grabbed.'
+					description: 'You add your level to your effective size for the purpose of interacting with creatures and objects, including determining whether you can lift an object, are affected by forced movement, and so forth. This has no effect on whether you can be grabbed.'
 				}),
 				FactoryLogic.feature.createBonus({
 					id: 'fury-sub-1-3-2',
 					field: FeatureField.Stability,
 					valueCharacteristics: [ Characteristic.Might ]
+				})
+			]
+		},
+		{
+			level: 4,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-1-4-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**8 Ferocity:** The first time you push a creature on a turn, you gain 2 surges.'
+				})
+			]
+		},
+		{
+			level: 5,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-1-5-1',
+					name: 'Bounder',
+					description: 'Your jump distance and height double (see Movement Types in Chapter 10: Combat). Additionally, when you fall, you reduce the effective height of your fall by a number of squares equal to your jump distance for the purpose of determining damage and whether you land prone (see Falling in Chapter 10). You are not prone after falling and landing on another creature.'
+				})
+			]
+		},
+		{
+			level:  6,
+			features: [
+				FactoryLogic.feature.createChoice({
+					id: 'fury-sub-1-6-1',
+					options: [
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-1-6-1a',
+									name: 'Avalanche Impact',
+									description: 'You leap and crash down, causing a shockwave that devastates foes.',
+									type: FactoryLogic.type.createManeuver(),
+									keywords: [ AbilityKeyword.Magic ],
+									distance: [ FactoryLogic.distance.createSelf() ],
+									target: 'Self',
+									cost: 9,
+									sections: [
+										FactoryLogic.createAbilitySectionText('You jump up to your maximum jump distance and make one power roll that targets each creature adjacent to the space where you land.'),
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '4 damage; push 1',
+												tier2: '7 damage; push 2',
+												tier3: '11 damage; push 3'
+											})
+										)
+									]
+								})
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-1-6-1b',
+									name: 'Force of Storms',
+									description: 'You strike an enemy hard enough to be a projectile that knocks a crowd of creatures around.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createMelee() ],
+									target: '1 creature',
+									cost: 9,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '7 + M damage; push 3',
+												tier2: '11 + M damage; push 5',
+												tier3: '16 + M damage; push 7'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('When the target ends this forced movement, each creature within 2 squares of the target is pushed 3 squares.')
+									]
+								})
+							}),
+							value: 1
+						}
+					]
+				})
+			]
+		},
+		{
+			level: 7,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-1-7-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**10 Ferocity:** You have a double edge on Might tests and the Knockback maneuver.'
+				})
+			]
+		},
+		{
+			level: 8,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-1-8-1',
+					name: 'Strongest There Is',
+					description: 'Your strength is unmatched. Whenever you make a Might test, you can roll three dice and choose which two to use. Additionally, whenever you use the Knockback maneuver, the forced movement distance gains a bonus equal to your Might score.'
+				})
+			]
+		},
+		{
+			level:  9,
+			features: [
+				FactoryLogic.feature.createChoice({
+					id: 'fury-sub-1-9-1',
+					options: [
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-1-9-1a',
+									name: 'Death Comes for You All!',
+									description: 'You use your weapon to create a destructive shockwave.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Area, AbilityKeyword.Magic, AbilityKeyword.Melee, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.create({ type: AbilityDistanceType.Burst, value: 3 }) ],
+									target: 'Each enemy in area',
+									cost: 11,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '7 damage; push 3',
+												tier2: '10 damage; push 5',
+												tier3: '15 damage; push 7'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('If this forced movement causes a target to be hurled through an object, that target takes an extra 10 damage.')
+									]
+								})
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-1-9-1b',
+									name: 'Primordial Vortex',
+									description: 'You channel the power of the Primordial Chaos to pull foes to you.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.create({ type: AbilityDistanceType.Burst, value: 3 }) ],
+									target: 'Each enemy in the area',
+									cost: 11,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '3 damage; vertical pull 3',
+												tier2: '5 damage; vertical pull 5',
+												tier3: '8 damage; vertical pull 7'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('If this forced movement causes a target to slam into you, you take no damage from the collision and the target takes the damage you would have taken.')
+									]
+								})
+							}),
+							value: 1
+						}
+					]
+				})
+			]
+		},
+		{
+			level: 10,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-1-10-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**12 Ferocity:** Whenever you use a heroic ability, you gain 10 temporary Stamina. Additionally, whenever you make a power roll that imposes forced movement on a target, the forced movement distance gains a bonus equal to your Might score.'
 				})
 			]
 		}
--- src/data/classes/fury/reaver.ts
+++ src/data/classes/fury/reaver.ts
@@ -27,26 +27,26 @@ export const reaver: SubClass = {
 					description: `
 You are never surprised. Additionally, whenever you would push a target with forced movement, you can slide them instead.
 
-As your rage grows, your primordial cunning intensifies. Benefits are cumulative except where an improved benefit replaces a lesser benefit.
+As your ferocity grows, you gain benefits as noted on the Reaver Growing Ferocity table. Benefits are cumulative except where an improved benefit replaces a lesser benefit.
 
-* **Rage 2**: Add your Agility to the distance you achieve on the Knockback maneuver.
-* **Rage 4**: Gain one surge the first time on a turn that you slide a creature.
-* **Rage 6**: Gain an edge on Agility tests and the Knockback maneuver.`
+* **Ferocity 2**: Whenever you use the Knockback maneuver, the forced movement distance gains a bonus equal to your Agility score.
+* **Ferocity 4**: The first time you slide a creature on a turn, you gain 1 surge.
+* **Ferocity 6**: You gain an edge on Agility tests and the Knockback maneuver.`
 				}),
 				FactoryLogic.feature.createAbility({
 					ability: FactoryLogic.createAbility({
 						id: 'fury-sub-2-1-4',
 						name: 'Unearthly Reflexes',
-						description: 'Elusive as a hummingbird.',
+						description: 'You are as elusive as a hummingbird.',
 						type: FactoryLogic.type.createTrigger('You take damage.'),
 						distance: [ FactoryLogic.distance.createSelf() ],
 						target: 'Self',
 						sections: [
-							FactoryLogic.createAbilitySectionText('You take half damage from the attack and can shift up to a number of squares equal to your Agility score.'),
+							FactoryLogic.createAbilitySectionText('You take half the damage from the triggering effect and can shift up to a number of squares equal to your Agility score.'),
 							FactoryLogic.createAbilitySectionField({
 								name: 'Spend',
 								value: 1,
-								effect: 'You reduce the potency of any effect associated with the damage for you by 1.'
+								effect: 'If the damage has any potency effects associated with it, the potency is reduced by 1 for you.'
 							})
 						]
 					})
@@ -79,34 +79,8 @@ As your rage grows, your primordial cunning intensifies. Benefits are cumulative
 							feature: FactoryLogic.feature.createAbility({
 								ability: FactoryLogic.createAbility({
 									id: 'fury-sub-2-2-2a',
-									name: 'Phalanx Breaker',
-									description: 'Organizing your forces like feckless creatures of Law. Pitiful.',
-									type: FactoryLogic.type.createMain(),
-									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
-									distance: [ FactoryLogic.distance.createSelf() ],
-									target: 'Self',
-									cost: 5,
-									sections: [
-										FactoryLogic.createAbilitySectionText('You shift up to your speed. You make one power roll that targets up to three enemies you come adjacent to during the shift.'),
-										FactoryLogic.createAbilitySectionRoll(
-											FactoryLogic.createPowerRoll({
-												characteristic: [ Characteristic.Might ],
-												tier1: '2 damage; A < [weak], dazed (save ends)',
-												tier2: '4 damage; A < [average], dazed (save ends)',
-												tier3: '6 damage; A < [strong], dazed (save ends)'
-											})
-										)
-									]
-								})
-							}),
-							value: 1
-						},
-						{
-							feature: FactoryLogic.feature.createAbility({
-								ability: FactoryLogic.createAbility({
-									id: 'fury-sub-2-2-2b',
-									name: 'RRRAAAGHH!',
-									description: 'Death! Deeaaath!!',
+									name: 'Death ... Deeaaath!',
+									description: 'Your unbridled rage strikes terror in their hearts.',
 									type: FactoryLogic.type.createMain(),
 									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
 									distance: [ FactoryLogic.distance.createMelee() ],
@@ -123,6 +97,32 @@ As your rage grows, your primordial cunning intensifies. Benefits are cumulative
 								})
 							}),
 							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-2-2-2b',
+									name: 'Phalanx-Breaker',
+									description: 'Organizing your forces like feckless creatures of Law. Pitiful.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createSelf() ],
+									target: 'Self; see below',
+									cost: 5,
+									sections: [
+										FactoryLogic.createAbilitySectionText('You shift up to your speed and make one power roll that targets up to three enemies you move adjacent to during this shift.'),
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '2 damage; A < [weak], dazed (save ends)',
+												tier2: '4 damage; A < [average], dazed (save ends)',
+												tier3: '6 damage; A < [strong], dazed (save ends)'
+											})
+										)
+									]
+								})
+							}),
+							value: 1
 						}
 					]
 				})
@@ -132,9 +132,175 @@ As your rage grows, your primordial cunning intensifies. Benefits are cumulative
 			level: 3,
 			features: [
 				FactoryLogic.feature.create({
-					id: 'fury-sub-1-3-1',
+					id: 'fury-sub-2-3-1',
 					name: 'See Through Your Tricks',
 					description: 'You have a double edge on tests made to search for hidden creatures, discern hidden motives, or detect lies. You also have a double edge on tests made to gamble!'
+				})
+			]
+		},
+		{
+			level: 4,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-2-4-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**8 Ferocity:** The first time you slide a creature on a turn, you gain 2 surges.'
+				})
+			]
+		},
+		{
+			level: 5,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-2-5-1',
+					name: 'Unfettered',
+					description: 'At the start of your turn, you can end any restrained condition on you. Additionally, you have a double edge on tests made to escape being confined or imprisoned.'
+				})
+			]
+		},
+		{
+			level:  6,
+			features: [
+				FactoryLogic.feature.createChoice({
+					id: 'fury-sub-2-6-1',
+					options: [
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-2-6-1a',
+									name: 'Death Strike',
+									description: 'Once you taste your foe’s blood, you become more efficient and turn every killing blow into an opportunity.',
+									type: FactoryLogic.type.createTrigger('You reduce a creature to 0 Stamina with a strike.', { free: true } ),
+									keywords: [ AbilityKeyword.Magic, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createMelee() ],
+									target: 'Self',
+									cost: 9,
+									sections: [
+										FactoryLogic.createAbilitySectionText('You target a creature adjacent to you with the same strike, using the same power roll as the triggering strike.')
+									]
+								})
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-2-6-1b',
+									name: 'Seek and Destroy',
+									description: 'You break through the enemy lines to make an example.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createMelee() ],
+									target: '1 creature',
+									cost: 9,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '4 + M damage; P < [weak] frightened (save ends)',
+												tier2: '6 + M damage; P < [average] frightened (save ends)',
+												tier3: '10 + M damage; P < [strong] frightened (save ends)'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('If a target who is not a leader or solo creature is winded by this strike, they are reduced to 0 Stamina and you choose an enemy within 5 squares of you. If that enemy has P < [average], they are frightened of you (save ends).')
+									]
+								})
+							}),
+							value: 1
+						}
+					]
+				})
+			]
+		},
+		{
+			level: 7,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-2-7-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**10 Ferocity:** You have a double edge on Agility tests and the Knockback maneuver.'
+				})
+			]
+		},
+		{
+			level: 8,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-2-8-1',
+					name: 'A Step Ahead',
+					description: 'You move with legendary grace. Whenever you make an Agility test, you can roll three dice and choose which two to use. Additionally, whenever you use the Disengage move action, the distance you can shift gains a bonus equal to your Agility score.'
+				})
+			]
+		},
+		{
+			level:  9,
+			features: [
+				FactoryLogic.feature.createChoice({
+					id: 'fury-sub-2-9-1',
+					options: [
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-2-9-1a',
+									name: 'Primordial Bane',
+									description: 'You attune the target to be weaker to a specific element.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Magic, AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createMelee() ],
+									target: '1 creature',
+									cost: 11,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '11 + M damage',
+												tier2: '16 + M damage',
+												tier3: '21 + M damage'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('Choose acid, cold, corruption, fire, lightning, poison, or sonic damage. The target loses any damage immunity to the chosen type and gains weakness 10 to the chosen type (save ends).')
+									]
+								})
+							}),
+							value: 1
+						},
+						{
+							feature: FactoryLogic.feature.createAbility({
+								ability: FactoryLogic.createAbility({
+									id: 'fury-sub-2-9-1b',
+									name: 'Shower of Blood',
+									description: 'You shock your foes with the brutality of your strike, resetting the balance of combat.',
+									type: FactoryLogic.type.createMain(),
+									keywords: [ AbilityKeyword.Melee, AbilityKeyword.Strike, AbilityKeyword.Weapon ],
+									distance: [ FactoryLogic.distance.createMelee() ],
+									target: '1 creature',
+									cost: 11,
+									sections: [
+										FactoryLogic.createAbilitySectionRoll(
+											FactoryLogic.createPowerRoll({
+												characteristic: [ Characteristic.Might ],
+												tier1: '12 + M damage',
+												tier2: '18 + M damage',
+												tier3: '24 + M damage'
+											})
+										),
+										FactoryLogic.createAbilitySectionText('Each enemy within 5 squares of you is distracted until the end of the round. While a creature is distracted this way, they can’t take triggered actions or free triggered actions, ability rolls made against them gain an edge, and their characteristic scores are considered 1 lower for the purpose of resisting potencies.')
+									]
+								})
+							}),
+							value: 1
+						}
+					]
+				})
+			]
+		},
+		{
+			level: 10,
+			features: [
+				FactoryLogic.feature.create({
+					id: 'fury-sub-2-10-1',
+					name: 'Growing Ferocity Improvement',
+					description: '**12 Ferocity:** Whenever you use a heroic ability, you gain 10 temporary Stamina. Additionally, whenever you make a power roll that imposes forced movement on a target, the forced movement distance gains a bonus equal to your Agility score.'
 				})
 			]
 		}

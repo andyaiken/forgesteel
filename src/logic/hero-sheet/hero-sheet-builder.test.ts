17cae8f6d5f89d1a2bf9fcd892c80a5607683b15
Veritas1000 <Veritas1000@users.noreply.github.com>
2025-11-25 13:37:07 -0500

    Fix retainers in classic sheet (#733)
    * Fix retainers missing from classic sheet
    
    * Fix beastheart companions

--- src/logic/hero-sheet/hero-sheet-builder.test.ts
+++ src/logic/hero-sheet/hero-sheet-builder.test.ts
@@ -1,17 +1,28 @@
-import { afterEach, describe, expect, test, vi } from 'vitest';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { FeatureCompanion, FeatureFollower, FeatureRetainer, FeatureSummonChoice, FeatureSummonChoiceData } from '@/models/feature';
+import { afterEach, describe, expect, expectTypeOf, test, vi } from 'vitest';
 import { FactoryLogic } from '@/logic/factory-logic';
-import { FeatureSummonChoice } from '@/models/feature';
+import { FeatureType } from '@/enums/feature-type';
+import { FollowerSheet } from '@/models/classic-sheets/hero-sheet';
+import { FollowerType } from '@/enums/follower-type';
+import { Hero } from '@/models/hero';
 import { HeroLogic } from '@/logic/hero-logic';
 import { HeroSheetBuilder } from '@/logic/hero-sheet/hero-sheet-builder';
+import { MonsterData } from '@/data/monster-data';
 import { MonsterRoleType } from '@/enums/monster-role-type';
+import { Options } from '@/models/options';
+import { Sourcebook } from '@/models/sourcebook';
 import { Summon } from '@/models/summon';
+import { beastheart } from '@/data/classes/beastheart/beastheart';
+import { highElfTactician } from '@/data/heroes/high-elf-tactician';
+import { retainer } from '@/data/monsters/retainer';
 import { undead } from '@/data/classes/summoner/undead';
 
-describe('buildSummonSheet', () => {
-	afterEach(() => {
-		vi.resetAllMocks();
-	});
+afterEach(() => {
+	vi.resetAllMocks();
+});
 
+describe('buildSummonSheet', () => {
 	vi.mock('@/logic/hero-logic', () => {
 		const HeroLogic = vi.fn();
 		return { HeroLogic: HeroLogic };
@@ -50,5 +61,131 @@ describe('buildSummonSheet', () => {
 		expect(sheet.weakness).toBe('');
 		expect(sheet.movement).toBe('');
 		expect(sheet.freeStrikeDamageType).toBe('');
+	});
+});
+
+// #region Mock data
+const mockHero = { id: 'test123' } as Hero;
+
+const mockArtisan = FactoryLogic.createFollower(FollowerType.Artisan);
+const mockFeatureFollower = {
+	id: 'mock-follower',
+	name: 'Mock Follower',
+	type: FeatureType.Follower,
+	data: {
+		follower: mockArtisan
+	}
+} as FeatureFollower;
+
+const testCompanionMonster = MonsterData.undead.monsters[0];
+const mockFeatureCompanion = {
+	id: 'mock-companion',
+	name: 'Mock Companion',
+	type: FeatureType.Companion,
+	data: {
+		selected: testCompanionMonster
+	}
+} as FeatureCompanion;
+
+const retainer1 = retainer.monsters[0];
+const mockFeatureRetainer = {
+	id: 'mock-retainer',
+	name: 'Mock Retainer',
+	type: FeatureType.Retainer,
+	data: {
+		selected: retainer1
+	}
+} as FeatureRetainer;
+
+const undeadSignatureChoices = undead.featuresByLevel.find(fbl => fbl.level === 1)
+	?.features.find(f => f.id === 'summoner-4-1-4')?.data as FeatureSummonChoiceData;
+const minionSummon1 = undeadSignatureChoices.options.find(o => o.monster.id === 'summoner-4-1-4a') as Summon;
+
+const beastheartCompanionChoices = beastheart.featuresByLevel.find(fbl => fbl.level === 1)
+	?.features.find(f => f.id === 'beastheart-1-2a')?.data as FeatureSummonChoiceData;
+const companion1 = beastheartCompanionChoices.options.find(o => o.monster.id === 'beastheart-1-2a-1') as Summon;
+
+const mockSummonChoiceFeature = {
+	id: 'mock-summon-chouice',
+	name: 'Mock Summon Choice',
+	type: FeatureType.SummonChoice,
+	data: {
+		selected: [ companion1, minionSummon1 ]
+	}
+} as FeatureSummonChoice;
+// #endregion
+
+describe('buildFollowerCompanionSheet()', () => {
+	test('it should call the correct builder method for Follower features', () => {
+		const mockResult = { id: 'foo' } as FollowerSheet;
+		const mockBuilderMethod = vi.spyOn(HeroSheetBuilder, 'buildFollowerSheet').mockReturnValueOnce(mockResult);
+
+		const result = HeroSheetBuilder.buildFollowerCompanionSheet(mockFeatureFollower, mockHero);
+
+		expect(mockBuilderMethod).toHaveBeenCalledExactlyOnceWith(mockArtisan);
+		expect(result).toBe(mockResult);
+	});
+
+	test('it should call the correct builder method for Companion features', () => {
+		const mockResult = { id: 'bar' } as FollowerSheet;
+		const mockBuilderMethod = vi.spyOn(HeroSheetBuilder, 'buildRetainerSheet').mockReturnValueOnce(mockResult);
+
+		const result = HeroSheetBuilder.buildFollowerCompanionSheet(mockFeatureCompanion, mockHero);
+
+		expect(mockBuilderMethod).toHaveBeenCalledExactlyOnceWith(testCompanionMonster);
+		expect(result).toBe(mockResult);
+	});
+
+	test('it should call the correct builder method for Retainer features', () => {
+		const mockResult = { id: 'bar' } as FollowerSheet;
+		const mockBuilderMethod = vi.spyOn(HeroSheetBuilder, 'buildRetainerSheet').mockReturnValueOnce(mockResult);
+
+		const result = HeroSheetBuilder.buildFollowerCompanionSheet(mockFeatureRetainer, mockHero);
+
+		expect(mockBuilderMethod).toHaveBeenCalledExactlyOnceWith(retainer1);
+		expect(result).toBe(mockResult);
+	});
+
+	test('it should call the correct builder method for Summoner selection features', () => {
+		const mockSheet1 = { id: 'companion-1' } as FollowerSheet;
+
+		const mockBuilderMethod = vi.spyOn(HeroSheetBuilder, 'buildCompanionSheet')
+			.mockReturnValueOnce(mockSheet1);
+
+		const result = HeroSheetBuilder.buildFollowerCompanionSheet(mockSummonChoiceFeature, mockHero);
+
+		expect(mockBuilderMethod).toHaveBeenCalledExactlyOnceWith(companion1, mockHero);
+		expect(result).toBeDefined();
+		expect(result).not.toBeNullable();
+
+		// @ts-ignore we are asserting all this stuff it's compaining about
+		expectTypeOf(result).toBeArray();
+		// @ts-ignore
+		expect(result.length).toBe(1);
+		// @ts-ignore
+		expect(result[0]).toBe(mockSheet1);
+	});
+});
+
+describe('buildHeroSheet', () => {
+	vi.unmock('@/logic/hero-logic');
+
+	test('it should build follower sheets for all correct types of follower/companion features', () => {
+		const hero = highElfTactician;
+		const sourcebooks: Sourcebook[] = [];
+		const options = {} as Options;
+
+		vi.spyOn(HeroLogic, 'getFeatures').mockReturnValue([
+			{ feature: mockFeatureRetainer, source: 'test' },
+			{ feature: mockFeatureFollower, source: 'test' },
+			{ feature: mockFeatureCompanion, source: 'test' },
+			{ feature: mockSummonChoiceFeature, source: 'test' }
+		]);
+
+		const result = HeroSheetBuilder.buildHeroSheet(hero, sourcebooks, options);
+
+		expect(result).toBeDefined();
+		expect(result).not.toBeNullable();
+		expect(result.followers.length).toBe(4);
 	});
 });

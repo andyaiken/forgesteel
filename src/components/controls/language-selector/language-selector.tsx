39cb45bcbf8fbda90c201d3dbf4cb1808c4bacb4
nicolo.castro <nicolo.castro89@gmail.com>
2026-01-10 22:33:28 +0100

    Use local selector and correctly handle detected language

--- src/components/controls/language-selector/language-selector.tsx
+++ src/components/controls/language-selector/language-selector.tsx
@@ -1,10 +1,34 @@
-import { ErrorBoundary } from '@/components/controls/error-boundary/error-boundary';
 import { Button, Dropdown, MenuProps } from 'antd';
+import { ErrorBoundary } from '@/components/controls/error-boundary/error-boundary';
 import i18next from 'i18next';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import '/node_modules/flag-icons/css/flag-icons.min.css';
 
 import './language-selector.scss';
-import { useTranslation } from 'react-i18next';
+
+function getCountryCode(lang?: string): string {
+	if (!lang) return 'gb';
+
+	const parts = lang.split('-');
+
+	// find the part that had length 2 (country code) or length 3 and is numeric (region code)
+	const region = parts.find(
+		part => part.length === 2 || (part.length === 3 && /^\d+$/.test(part))
+	);
+
+	if (region) {
+		return region.toLowerCase();
+	}
+
+	const fallbackMap: Record<string, string> = {
+		en: 'gb',
+		it: 'it'
+	};
+
+	return fallbackMap[parts[0]] ?? 'gb';
+}
 
 export const LanguageSelector = () => {
 	const { i18n } = useTranslation();
@@ -33,10 +57,15 @@ export const LanguageSelector = () => {
 		items,
 		onClick: handleMenuClick
 	};
+	const countryCode = useMemo(
+		() => getCountryCode(i18n.resolvedLanguage),
+		[ i18n.resolvedLanguage ]
+	);
+
 	return (
 		<ErrorBoundary>
 			<Dropdown menu={menuProps}>
-				<Button shape='circle' className={`fi fi-${i18n.language.split('-')[1]?.toLowerCase() ?? 'gb'}`} />
+				<Button shape='circle' className={`fi fi-${countryCode}`} />
 			</Dropdown>
 		</ErrorBoundary>
 	);

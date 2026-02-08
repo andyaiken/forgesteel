8655d29e4b08f89476f36df4732aa7c4d3ce5eb4
Minhajul Hoq <minhoq@gmail.com>
2026-02-08 16:48:30 -0500

    refactor: class specialization display logic in HeroPanel to account for Conduits Domains as Subclasses

--- src/components/panels/hero/hero-panel.tsx
+++ src/components/panels/hero/hero-panel.tsx
@@ -634,12 +634,7 @@ export const HeroPanel = (props: Props) => {
 						props.hero.class ?
 							useRows ?
 								<div className='selectable-row clickable' onClick={onSelectClass}>
-									{
-										props.hero.class.subclasses.filter(sc => sc.selected).length > 0 ?
-											<div>Class: <b>{props.hero.class.name} ({props.hero.class.subclasses.filter(sc => sc.selected).map(sc => sc.name).join(' ')}, level {props.hero.class.level})</b></div>
-											:
-											<div>Class: <b>{props.hero.class.name} (level {props.hero.class.level})</b></div>
-									}
+									<div>Class: <b>{props.hero.class.name} ({[`level ${props.hero.class.level}`, ...HeroLogic.getClassSpecialization(props.hero)].join(' ')})</b></div>
 								</div>
 								:
 								<div className='overview-tile clickable' onClick={onSelectClass}>
@@ -647,8 +642,8 @@ export const HeroPanel = (props: Props) => {
 									<Field label='Class' value={props.hero.class.name} />
 									<Field label='Level' value={props.hero.class.level} />
 									{
-										props.hero.class.subclasses.filter(sc => sc.selected).length > 0 ?
-											<Field label={props.hero.class.subclassName} value={props.hero.class.subclasses.filter(sc => sc.selected).map(sc => sc.name).join(', ') || ''} />
+										HeroLogic.getClassSpecialization(props.hero).length > 0 ?
+											<Field label={props.hero.class.subclassName || 'Domains'} value={HeroLogic.getClassSpecialization(props.hero).join(', ')} />
 											: null
 									}
 								</div>
@@ -1160,7 +1155,7 @@ export const HeroPanel = (props: Props) => {
 						props.hero.class ?
 							<Field
 								label='Class'
-								value={`${props.hero.class.name} (${[ `Level ${props.hero.class.level}`, ...props.hero.class.subclasses.filter(sc => sc.selected).map(sc => sc.name) ].join(' ')})`}
+								value={`${props.hero.class.name} (${[`Level ${props.hero.class.level}`, ...HeroLogic.getClassSpecialization(props.hero)].join(' ')})`}
 							/>
 							: null
 					}
